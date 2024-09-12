65b81ffe4bbb46d4eef1d71fc3abd5fc7b24a352
Patrick Odeh <vyltyan@gmail.com>
2024-09-12 20:16:57 +0100

    monty

--- opcode.c
+++ opcode.c
@@ -1,28 +1,6 @@
 #include "monty.h"
 
 /**
- * pop - Removes the top element of the stack.
- * @stack: Double pointer to the head of the stack.
- * @line_number: The current line number in the Monty bytecode file.
- */
-void pop(stack_t **stack, unsigned int line_number)
-{
-	stack_t *temp;
-
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	temp = *stack;
-	*stack = (*stack)->next;
-	if (*stack != NULL)
-		(*stack)->prev = NULL;
-	free(temp);
-}
-
-/**
  * read_file - Reads and processes the Monty bytecode file line by line.
  * @file: The file pointer to the Monty bytecode file.
  */
